89fa293ac9bcc1914d8b883c235edfb43c83ef85
Mayara Cardoso <mayarakardoso@gmail.com>
2023-12-20 20:16:22 -0300

    feat: cria app com header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent],
+    schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppComponent {
   title = 'spotify-imersao-angular';
+
 }
